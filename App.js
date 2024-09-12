4ebeb20e0cbbb40e5f8b18cbb78653ac9c39176f
Luissalamanca23 <l.salamanca18@icloud.com>
2024-09-12 18:31:55 -0300

    feat: Update App.js to include LoginScreen component and navigation
    The code changes in App.js add the LoginScreen component and implement navigation using react-navigation. The LoginScreen component handles user login functionality, allowing users to enter their username and password, validating the input, and displaying appropriate alerts. The navigation is set up using createStackNavigator from react-navigation, with the initial route set to "Login". This commit message follows the established convention of using a prefix to indicate the type of change (feat for a new feature) and provides a clear and concise description of the changes made.

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-// App.js
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Provider as PaperProvider } from 'react-native-paper';
+
+// Importar las pantallas de tu aplicación
 import Homepage from './components/Homepage';
+import LoginScreen from './components/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
-      <Homepage />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login" // Define la primera pantalla que se mostrará al iniciar la aplicación
+          screenOptions={{
+            headerShown: false, // Puedes ocultar el encabezado para todas las pantallas si prefieres un diseño limpio
+          }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={Homepage} />
+          {/* Agrega más pantallas aquí según sea necesario */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
